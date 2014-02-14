2fb9a0979e33a01e2a2e1e554f7145009bacf0eb
Moritz Emanuel Beber <beber@molgen.mpg.de>
2014-02-14 16:58:47 +0100

    document the cpdb_client
    * changed the catching of errors slightly
    * further tests into pysimplesoap

--- cpdb_client.py
+++ cpdb_client.py
@@ -22,13 +22,12 @@ Threaded Use of CPDB SOAP/WSDL API
 __all__ = ["WSDLQueryThread"]
 
 
-# stdlib
 import logging
 import threading
-# stdlib
+
 from uuid import uuid4
 from Queue import Queue
-# external
+
 from suds.client import Client, WebFault
 
 
@@ -36,31 +35,50 @@ LOGGER = logging.getLogger(__name__)
 
 
 class WSDLQueryThread(threading.Thread):
-    """
-
-    """
     sentinel = uuid4()
 
-    def __init__(self, wsdl, task_queue=None, results=None, wsdl_kw_args={},
-            **kw_args):
+    def __init__(self, wsdl, task_queue=Queue(), results=list(),
+            wsdl_kw_args=dict(), **kw_args):
         """
+        Thread that retrieves SOAP queries from a queue and appends results.
+
+        Parameters
+        ----------
+        wsdl: str
+            URL of the WSDL file.
+        task_queue: queue.Queue
+            Any object that supports the ``get`` and ``task_done`` methods.
+        results: list
+            Any container that supports the ``append`` method.
+        wsdl_kw_args: dict
+            Passed on to the suds.client.Client constructor.
+
+        Note
+        ----
+        This thread does not need to be daemonized, it can be terminated
+        gracefully by putting the class sentinel in the queue.
+        The task_queue and results default arguments are intended so that they
+        are the same objects across all instances.
         """
         super(WSDLQueryThread, self).__init__(**kw_args)
         self.client = Client(wsdl, **wsdl_kw_args)
-        self.task_queue = Queue() if task_queue is None else task_queue
-        self.results = list() if results is None else results
+        self.task_queue = task_queue
+        self.results = results
         self._lock = threading.Lock()
 
     def run(self):
         """
+        Called by the ``start`` method.
         """
         for task in iter(self.task_queue.get, self.sentinel):
-            (func, args, kw_args) = task
-            LOGGER.debug("task found '%s'", str(func))
             try:
+                (func, args, kw_args) = task
+                LOGGER.debug("task found '%s'", str(func))
                 response = getattr(self.client.service, func)(*args, **kw_args)
             except WebFault as err:
-                LOGGER.warn(str(err))
+                LOGGER.error("miscommunication with server: '%s'", str(err))
+            except StandardError as err:
+                LOGGER.error(str(err))
             else:
                 with self._lock:
                     self.results.append(response)
